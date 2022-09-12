765a204cbbb1039a8580c9c392e20bbb8a58e1f1
Nik-McKnight <nik.mcknight735@gmail.com>
2022-09-12 14:05:04 -0600

    initialized components and set up router

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+export { default as About } from "./About";
+export { default as App } from "./App";
+export { default as Contact } from "./Contact";
+export { default as Education } from "./Education";
+export { default as Footer } from "./Footer";
+export { default as Header } from "./Header";
+export { default as Home } from "./Home";
+export { default as NavBar } from "./NavBar";
+export { default as Projects } from "./Projects";
+export { default as Skills } from "./Skills";

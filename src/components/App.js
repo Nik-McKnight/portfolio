485c76f45eea8c0676cee04dac3335b378593895
Nik-McKnight <nik.mcknight735@gmail.com>
2022-09-15 13:36:52 -0600

    set up routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,28 @@
-import logo from "../logo.svg";
 import "../style/App.css";
+import Header from "./Header";
+import Footer from "./Footer";
+import Home from "./Home";
+import { Routes, Route } from "react-router-dom";
+import About from "./About";
+import Projects from "./Projects";
+import Education from "./Education";
+import Skills from "./Skills";
+import Contact from "./Contact";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/projects" element={<Projects />}></Route>
+        <Route path="/education" element={<Education />}></Route>
+        <Route path="/skills" element={<Skills />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/*" element={<Home />} />
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { useEffect } from "react";
 
 export default function Home() {
-  return <div>Home</div>;
+  useEffect(() => {
+    window.history.replaceState(null, "", "/");
+  }, []);
+
+  return (
+    <>
+      <div>Home</div>
+    </>
+  );
 }

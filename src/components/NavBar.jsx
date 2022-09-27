b3bf6c8b4c538654c6c3da8fa85290146feb71f0
Nik-McKnight <nik.mcknight735@gmail.com>
2022-09-27 12:31:41 -0600

    Navbar is now responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,35 +11,35 @@ const toggleMenu = ({ isOpen }) => {
 
 export default function NavBar() {
   return (
-    // <nav>
-    //   <Link to="/">Home</Link>
-    //   <Link to="/about">About Me</Link>
-    //   <Link to="/projects">Projects</Link>
-    //   <Link to="/education">Education</Link>
-    //   <Link to="/skills">Skills</Link>
-    //   <Link to="/contact">Contact</Link>
-    // </nav>
-    <Menu noOverlay onStateChange={toggleMenu}>
-      <Link className="menu-item" to="/">
-        Home
-      </Link>
-      <Link className="menu-item" to="/about">
-        About Me
-      </Link>
-      <Link className="menu-item" to="/projects">
-        Projects
-      </Link>
-      <Link className="menu-item" to="/education">
-        Education
-      </Link>
-      <Link className="menu-item" to="/skills">
-        Skills
-      </Link>
-      <Link className="menu-item" to="/contact">
-        Contact
-      </Link>
-    </Menu>
+    <>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About Me</Link>
+        <Link to="/projects">Projects</Link>
+        <Link to="/education">Education</Link>
+        <Link to="/skills">Skills</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+      <Menu noOverlay onStateChange={toggleMenu}>
+        <Link className="menu-item" to="/">
+          Home
+        </Link>
+        <Link className="menu-item" to="/about">
+          About Me
+        </Link>
+        <Link className="menu-item" to="/projects">
+          Projects
+        </Link>
+        <Link className="menu-item" to="/education">
+          Education
+        </Link>
+        <Link className="menu-item" to="/skills">
+          Skills
+        </Link>
+        <Link className="menu-item" to="/contact">
+          Contact
+        </Link>
+      </Menu>
+    </>
   );
-
-  /* Figure out the hamburger menu*/
 }

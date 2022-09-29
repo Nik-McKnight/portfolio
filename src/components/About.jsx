2e2c99a90cdbe2398bdd1d0c1c3286191f47896a
Nik-McKnight <nik.mcknight735@gmail.com>
2022-09-29 13:22:32 -0600

    partially filled page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,26 +3,30 @@ import React from "react";
 export default function About() {
   return (
     <div class="content">
-      About Me
-      <div>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo
-        dolore aperiam fuga, exercitationem molestias, porro cum molestiae
-        necessitatibus aspernatur dolor quia facilis neque nulla corrupti
-        corporis labore totam ipsa a?
+      <h1>About Me</h1>
+      <div className="about">
+        <h2>Bio</h2>
+        <div>
+          I grew up in Iowa City, Iowa, graduated from a technical college, and
+          worked as a fire alarm systems designer for most of my 20s. In 2019, I
+          made the decision to change careers. I worked through most of the
+          Computer Science program at the University of Missouri - Kansas City
+          (with excellent grades and while working 40+ hours per week) until I
+          had to leave for reasons related to the COVID-19 pandemic. A year
+          later, having settled in the Salt Lake City area, I learned the
+          remaining necessary skills to be a full stack developer by attending
+          Fullstack Academy.
+        </div>
       </div>
-      <div>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo
-        dolore aperiam fuga, exercitationem molestias, porro cum molestiae
-        necessitatibus aspernatur dolor quia facilis neque nulla corrupti
-        corporis labore totam ipsa a?
+      <div className="about">
+        <h2>Hobbies</h2>
+        fish games reading
       </div>
-      <div>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo
-        dolore aperiam fuga, exercitationem molestias, porro cum molestiae
-        necessitatibus aspernatur dolor quia facilis neque nulla corrupti
-        corporis labore totam ipsa a?
+      <div className="about">
+        <h2>Other Interests</h2>
+        sports movies history cats
       </div>
-      <div>
+      <div className="about">
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo
         dolore aperiam fuga, exercitationem molestias, porro cum molestiae
         necessitatibus aspernatur dolor quia facilis neque nulla corrupti

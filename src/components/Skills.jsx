24318927429479d620859c82ac156e0ca79ae7f5
Nik-McKnight <nik.mcknight735@gmail.com>
2022-10-03 14:48:04 -0600

    added skills; working on styling

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,30 +3,57 @@ import React from "react";
 export default function Skills() {
   return (
     <div class="content">
-      My Skills
-      <div>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo
-        dolore aperiam fuga, exercitationem molestias, porro cum molestiae
-        necessitatibus aspernatur dolor quia facilis neque nulla corrupti
-        corporis labore totam ipsa a?
+      <h1>Skills</h1>
+      <div className="category">
+        <h2>Full Stack Web Development</h2>
+        <ul className="skills">
+          <li>React</li>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>Javascript</li>
+          <li>Node.js</li>
+          <li>PostgreSQL</li>
+          <li>Express.js</li>
+          <li>Unit Testing</li>
+          <li>APIs</li>
+          <li>REST</li>
+          <li>JSON</li>
+          <li>Git</li>
+          <li>Data Structures</li>
+          <li>Algorithms</li>
+          <li>Object-Oriented Programming</li>
+          <li>Agile Design</li>
+          <li>Scrum</li>
+        </ul>
       </div>
-      <div>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo
-        dolore aperiam fuga, exercitationem molestias, porro cum molestiae
-        necessitatibus aspernatur dolor quia facilis neque nulla corrupti
-        corporis labore totam ipsa a?
+      <div className="category">
+        <h2>Other Software Development</h2>
+        <ul className="skills">
+          <li>Java</li>
+          <li>Data Structures</li>
+          <li>Algorithms</li>
+          <li>C#</li>
+          <li>Git</li>
+          <li>Object-Oriented Programming</li>
+          <li>SQL</li>
+          <li>C</li>
+          <li>Agile Design</li>
+          <li>Scrum</li>
+          <li>C++</li>
+          <li>Python</li>
+          <li>Unit Testing</li>
+        </ul>
       </div>
-      <div>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo
-        dolore aperiam fuga, exercitationem molestias, porro cum molestiae
-        necessitatibus aspernatur dolor quia facilis neque nulla corrupti
-        corporis labore totam ipsa a?
-      </div>
-      <div>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo
-        dolore aperiam fuga, exercitationem molestias, porro cum molestiae
-        necessitatibus aspernatur dolor quia facilis neque nulla corrupti
-        corporis labore totam ipsa a?
+      <div className="category">
+        <h2>Other</h2>
+        <ul className="skills">
+          <li>Problem Solving</li>
+          <li>Communication</li>
+          <li>Self-learning</li>
+          <li>Accountability</li>
+          <li>Time Management</li>
+          <li>Attention to Detail</li>
+        </ul>
       </div>
     </div>
   );
